51fbd51739e02235d99f19f3ea435bf65f2ee066
Ewann <ewannpv@google.com>
2021-05-14 16:06:20 +0200

    Adds hooks

--- src/components/home/containers/message-input-container.js
+++ src/components/home/containers/message-input-container.js
@@ -1,63 +1,54 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import {
   Col, Form, Button, Container, Row
 } from 'react-bootstrap';
-import { connect } from 'react-redux';
 import { SendMessage } from '../actions';
 import parseMessage from '../actions/message-parser';
 
-const MessageInputContainer = class MessageInputContainer extends Component {
-  constructor() {
-    super();
-    this.state = { textInput: '' };
+const MessageInputContainer = () => {
+  const dispatch = useDispatch();
+  const [state, setState] = useState({
+    textInput: ''
+  });
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
+  const data = useSelector((storeState) => storeState.chatBot);
+  const handleChange = (e) => {
+    setState({ textInput: e.target.value });
+  };
 
-  handleChange(e) {
-    this.setState({ textInput: e.target.value });
-  }
-
-  handleClick(e) {
-    const { dispatch } = this.props;
-    const { data } = this.props;
-
+  const handleClick = (e) => {
     e.preventDefault();
-    const { textInput } = this.state;
+    const { textInput } = state;
     if (!textInput) return;
     dispatch(SendMessage(textInput));
     parseMessage(data, textInput);
-    this.setState({ textInput: '' });
-  }
+    setState({ textInput: '' });
+  };
 
-  render() {
-    const { textInput } = this.state;
-    return (
-      <Col className="mt-3 pl-3 pr-3 message-input-container">
-        <Container fluid="xl" className="h-100">
-          <Row className="h-100">
-            <Col xl={3} />
-            <Col xl={9} className="p-0">
-              <Form>
-                <Form.Row className="d-flex">
-                  <Col className="d-flex">
-                    <Form.Control value={textInput} onChange={this.handleChange} type="text" placeholder="Write a message.." className="bg-white fs-5" />
-                  </Col>
-                  <Col className="input-btn-row p-0">
-                    <Button onKeyPress={(e) => e.preventDefault()} onClick={this.handleClick} variant="btn btn-outline-dark" type="submit">
-                      Send
-                    </Button>
-                  </Col>
-                </Form.Row>
-              </Form>
-            </Col>
-          </Row>
-        </Container>
-      </Col>
-    );
-  }
+  return (
+    <Col className="mt-3 pl-3 pr-3 message-input-container">
+      <Container fluid="xl" className="h-100">
+        <Row className="h-100">
+          <Col xl={3} />
+          <Col xl={9} className="p-0">
+            <Form>
+              <Form.Row className="d-flex">
+                <Col className="d-flex">
+                  <Form.Control value={state.textInput} onChange={handleChange} type="text" placeholder="Write a message.." className="bg-white fs-5" />
+                </Col>
+                <Col className="input-btn-row p-0">
+                  <Button onKeyPress={(e) => e.preventDefault()} onClick={handleClick} variant="btn btn-outline-dark" type="submit">
+                    Send
+                  </Button>
+                </Col>
+              </Form.Row>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </Col>
+  );
 };
 
-const mapTopProps = (store) => ({ data: store.chatBot });
-export default connect(mapTopProps)(MessageInputContainer);
+export default MessageInputContainer;
