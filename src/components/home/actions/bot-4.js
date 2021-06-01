f6a6144dfbd8096ebc1ebf05c302950907cdbdac
Ewann <ewannpv@google.com>
2021-06-01 14:25:30 +0200

    Fixes bot4 helper

--- src/components/home/actions/bot-4.js
+++ src/components/home/actions/bot-4.js
@@ -1,6 +1,6 @@
 import { SendResponse } from '../actions';
 import store from '../../../store';
-import getHelpBot4 from '../containers/helper-factory';
+import { getHelpBot4 } from '../containers/helper-factory';
 
 const handleBot4 = (bot) => {
   store.dispatch(SendResponse(getHelpBot4(), bot.key));
